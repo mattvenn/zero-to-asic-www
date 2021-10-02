37ffe1dee601638c08c80651ff5d033d1d68d7e3
matt venn <matt@mattvenn.net>
2021-10-02 12:00:33 +0200

    ignore secrets

---
+++ decrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output secrets.py secrets.py.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output tweet-a-term-stats-d8b4ef62a65c.json tweet-a-term-stats-d8b4ef62a65c.json.gpg
