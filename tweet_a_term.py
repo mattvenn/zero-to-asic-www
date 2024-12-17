2d6851841f78b09c7031f799f16034366b717f52
matt venn <matt@mattvenn.net>
2024-12-17 19:33:22 +0100

    start adding bluesky support

--- tweet_a_term.py
+++ tweet_a_term.py
@@ -4,6 +4,7 @@ import argparse
 from secrets import *
 import tweepy
 import glob
+from atproto import Client
 from num2words import num2words
 from analytics import run_report
 import os
@@ -22,8 +23,6 @@ class Term():
         self.url_end = self.file.replace('content/terminology/', '')
         self.url_end = self.url_end.lower().replace('.md', '')
         self.url = URL + "/terminology/" + self.url_end
-        # add tracking
-        self.url += '?utm_source=twitter&utm_medium=tweet&utm_campaign=terminology'
 
         self.term_rank = None
         rank = 0
@@ -57,8 +56,14 @@ class Term():
 
 
         ordinal_rank = num2words(self.term_rank, to="ordinal_num")
+        # add tracking
         self.twitter_text = "%s is the #ASIC terminology of the week!\n%s\n\nIn the last month, %s has been the %s most popular out of %d terms." % (
-            self.title, self.url, self.title, ordinal_rank, self.num_terms)
+            self.title, self.url + '?utm_source=twitter&utm_medium=tweet&utm_campaign=terminology', self.title, ordinal_rank, self.num_terms)
+        from atproto import client_utils
+        self.skeet_text = client_utils.TextBuilder()
+        self.skeet_text.text("%s is the #ASIC terminology of the week!\n\n" % self.title)
+        self.skeet_text.link(self.url, self.url + '?utm_source=bluesky&utm_medium=tweet&utm_campaign=terminology')
+        self.skeet_text.text("\n\nIn the last month, %s has been the %s most popular out of %d terms." % (self.title, ordinal_rank, self.num_terms))
 
     def __str__(self):
         return "%-40s %-20s %3s %s" % (self.file, self.title, self.term_rank, self.url)
@@ -81,9 +86,18 @@ class Term():
         response = client.create_tweet( text=self.twitter_text)
         print(response)
 
+    def skeet(self):
+        from atproto import Client
+
+        client = Client()
+        client.login('mattvenn.net', 'password')
+        post = client.send_post(self.skeet_text)
+        print(post)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="tweet a term")
     parser.add_argument('--tweet', help="actually do the tweet", action='store_const', const=True)
+    parser.add_argument('--skeet', help="actually do the skeet", action='store_const', const=True)
     parser.add_argument('--week', help="force a specific week", type=int)
     parser.add_argument('--week-offset', help="add this number to the week number", type=int, default=0)
     parser.add_argument('--test-url', help="check url is ok", action='store_const', const=True)
@@ -126,3 +140,6 @@ if __name__ == '__main__':
 
     if args.tweet:
         term_of_the_week.tweet()
+
+    if args.skeet:
+        term_of_the_week.skeet()
